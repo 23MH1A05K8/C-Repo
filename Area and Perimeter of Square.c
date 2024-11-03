677b35641cfc68dfd1170319480079ad55d08144
kavyanjali <147487908+23MH1A05K8@users.noreply.github.com>
2024-11-03 20:20:13 +0530

    Uploading code: Area and Perimeter of Square.c

---
+++ Area and Perimeter of Square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+    int a;
+    scanf("%d",&a);
+    int b=pow(a,2);
+    printf("%d ",b);
+    int c=4*a;
+    scanf("%d",&c);
+    printf("%d",c);
+}
